88fd063d813ff91f980676c25fbc00888689a494
SuperSolik <serpetr99@gmail.com>
2021-03-15 21:27:31 +0300

    added start test layout and activity

---
+++ app/src/main/java/com/pjs/itinterviewtrainer/StartTestActivity.kt
@@ -0,0 +1,11 @@
+package com.pjs.itinterviewtrainer
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class StartTestActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_start_test)
+    }
+}
