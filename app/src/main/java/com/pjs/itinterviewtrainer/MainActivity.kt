f68eddf6053ffcf8021ef6767becbc65873469f4
SuperSolik <serpetr99@gmail.com>
2021-03-15 21:23:40 +0300

    added start test layout and activity

--- app/src/main/java/com/pjs/itinterviewtrainer/MainActivity.kt
+++ app/src/main/java/com/pjs/itinterviewtrainer/MainActivity.kt
@@ -1,11 +1,17 @@
 package com.pjs.itinterviewtrainer
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        startTestBtn.setOnClickListener {
+            val intent = Intent(this, StartTestActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
