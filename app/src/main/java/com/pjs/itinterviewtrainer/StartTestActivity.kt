f6ea0b58b709222b5cf0d76a81cab6458e42d09d
SuperSolik <serpetr99@gmail.com>
2021-03-16 16:42:30 +0300

    renamed, added init layouts for categories, category tests, and items

--- app/src/main/java/com/pjs/itinterviewtrainer/StartTestActivity.kt
+++ app/src/main/java/com/pjs/itinterviewtrainer/StartTestActivity.kt
@@ -6,6 +6,6 @@ import android.os.Bundle
 class StartTestActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_start_test)
+        setContentView(R.layout.test_settings)
     }
 }
