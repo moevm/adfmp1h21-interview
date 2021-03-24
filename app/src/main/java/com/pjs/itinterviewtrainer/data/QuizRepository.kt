7c2857997671658285a2efa6f9d61b4901ce2547
Antay <vandervise465@gmail.com>
2021-03-24 18:54:54 +0300

    add mock data in repostiry

--- app/src/main/java/com/pjs/itinterviewtrainer/data/QuizRepository.kt
+++ app/src/main/java/com/pjs/itinterviewtrainer/data/QuizRepository.kt
@@ -7,6 +7,15 @@ import java.io.InputStream
 object QuizRepository {
     var categoriesList: List<QuestionCategory> = listOf("Python", "C++", "JavaScript").mapIndexed { i, c -> QuestionCategory(i, c) }
     var levelsList: List<QuestionLevel> = listOf("easy", "medium", "hard").mapIndexed { i, d -> QuestionLevel(i, d) }
+    var statisticsList: List<QuizResults> = listOf(
+            QuizResults(0, 23, 8, mutableListOf<Question>(), 2493, "Title"),
+            QuizResults(0, 13, 5, mutableListOf<Question>(), 493, "Test's title c++"),
+            QuizResults(0, 12, 9, mutableListOf<Question>(), 12345, "Long test"),
+            QuizResults(0, 15, 0, mutableListOf<Question>(), 43, "Short test"),
+            QuizResults(0, 23, 8, mutableListOf<Question>(), 300, "Traktorist"),
+            QuizResults(0, 43, 12, mutableListOf<Question>(), 839, "Test 2"),
+            QuizResults(0, 0, 0, mutableListOf<Question>(), 0, "Empty")
+    )
     lateinit var questionsList: List<Question>
     lateinit var quizList: List<Quiz>
 
