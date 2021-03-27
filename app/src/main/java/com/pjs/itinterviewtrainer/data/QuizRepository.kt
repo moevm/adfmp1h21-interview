2fd3158514e6268bc0ebe82b7a11378c1557b5f4
SuperSolik <serpetr99@gmail.com>
2021-03-28 00:45:51 +0300

    added saving stats to QuizRepository

--- app/src/main/java/com/pjs/itinterviewtrainer/data/QuizRepository.kt
+++ app/src/main/java/com/pjs/itinterviewtrainer/data/QuizRepository.kt
@@ -3,27 +3,15 @@ package com.pjs.itinterviewtrainer.data
 import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
 import java.io.InputStream
+import kotlin.math.min
 
 object QuizRepository {
     var categoriesList: List<QuestionCategory> =
         listOf("Python", "C++", "JavaScript").mapIndexed { i, c -> QuestionCategory(i + 1, c) }
     var levelsList: List<QuestionLevel> =
         listOf("Easy", "Medium", "Hard").mapIndexed { i, d -> QuestionLevel(i + 1, d) }
-    var statisticsList: List<QuizResults> = listOf(
-        QuizResults(0, 23, 8, mutableListOf<Question>(), 2493, "Title"),
-        QuizResults(0, 13, 5, mutableListOf<Question>(), 493, "Test's title c++ Long name"),
-        QuizResults(0, 12, 9, mutableListOf<Question>(), 12345, "Long test"),
-        QuizResults(0, 15, 0, mutableListOf<Question>(), 43, "Short test"),
-        QuizResults(0, 23, 8, mutableListOf<Question>(), 300, "Traktorist"),
-        QuizResults(0, 43, 12, mutableListOf<Question>(), 839, "Test 2"),
-        QuizResults(0, 0, 0, mutableListOf<Question>(), 0, "Empty"),
-        QuizResults(0, 13, 5, mutableListOf<Question>(), 493, "Test's title c++"),
-        QuizResults(0, 12, 9, mutableListOf<Question>(), 12345, "Long test"),
-        QuizResults(0, 15, 0, mutableListOf<Question>(), 43, "Short test"),
-        QuizResults(0, 23, 8, mutableListOf<Question>(), 300, "Traktorist"),
-        QuizResults(0, 43, 12, mutableListOf<Question>(), 839, "Test 2"),
-        QuizResults(0, 0, 0, mutableListOf<Question>(), 0, "Empty"),
-    )
+    var statisticsList: MutableList<QuizResults> = mutableListOf()
+
     lateinit var questionsList: List<Question>
     lateinit var quizList: List<Quiz>
 
@@ -38,17 +26,22 @@ object QuizRepository {
     fun createBasicsQuiz(categoryId: Int): Quiz {
         val categories =
             listOf(categoriesList.find { c -> c.id == categoryId } ?: categoriesList.first())
+
+        val basicQuestions = questionsList.filter { q ->
+                categories.map { it.id }.contains(q.category_id) && q.level_id == levelsList.first().id
+            }
+
+        val (picQuestions, noPicQuestions) = basicQuestions.partition { it.code_pic != null }
+
+        val questions = picQuestions.take(5).toMutableList()
+        questions.addAll(noPicQuestions.take(10 - questions.size))
+
         return Quiz(
             0,
             "Basics",
             categories,
             levelsList.first(),
-            questionsList
-                .filter { q ->
-                    categories.map { it.id }.contains(q.category_id) && q.code_pic != null
-//                        .contains(q.category_id) && q.level_id == levelsList.first().id
-                }
-                .take(10)
+            questions.shuffled()
         )
     }
 
@@ -57,7 +50,8 @@ object QuizRepository {
         categoriesList: Collection<QuestionCategory>,
         amount: Int
     ): List<Question> {
-        return questionsList.shuffled().toList().filter { q -> categoriesList.map { it.id }.contains(q.category_id) && q.level_id <= level.id}.take(amount)
+        val questions = questionsList.shuffled().toList().filter { q -> categoriesList.map { it.id }.contains(q.category_id) && q.level_id <= level.id}
+        return questions.take(min(amount, questions.size))
     }
 
 }
