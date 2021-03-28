738adf77031fa7334d96c6f110534f235b15e062
SuperSolik <serpetr99@gmail.com>
2021-03-28 12:45:06 +0300

    fixed back pressed on fragments in main activity

--- app/src/main/java/com/pjs/itinterviewtrainer/MainActivity.kt
+++ app/src/main/java/com/pjs/itinterviewtrainer/MainActivity.kt
@@ -1,9 +1,8 @@
 package com.pjs.itinterviewtrainer
 
-import android.content.Context
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.android.volley.toolbox.Volley
+import com.google.android.material.theme.overlay.MaterialThemeOverlay
 import com.pjs.itinterviewtrainer.data.QuizRepository
 
 class MainActivity : AppCompatActivity() {
@@ -26,6 +25,14 @@ class MainActivity : AppCompatActivity() {
         with(supportFragmentManager.beginTransaction()) {
             replace(R.id.container, MainFragment.newInstance(), MainFragment.TAG)
             commit()
+        }
+    }
+
+    override fun onBackPressed() {
+        if (supportFragmentManager.backStackEntryCount > 0) {
+            supportFragmentManager.popBackStack()
+        } else {
+            moveTaskToBack(true)
         }
     }
 }
